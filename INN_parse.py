fd5fe81ae79595c6863ed35301116929ca17d9fa
ravshan <r.gaynulin@gmail.com>
2024-11-22 13:29:04 +0300

    refactoring code

--- INN_parse.py
+++ INN_parse.py
@@ -1,68 +1,74 @@
 from dadata import Dadata
 from datetime import datetime
 import pandas as pd
+from tqdm import tqdm
 
 
-with open('C:\\!Равшан_работа\\OMT Consult\\Карты\\inn.txt', 'r',  encoding='utf-8-sig') as w:
-    a = []
+def safe_get(data, *keys, default=""):
+    """
+    Безопасное получение вложенного значения из словаря.
+    :param data: словарь или список
+    :param keys: путь к значению
+    :param default: значение по умолчанию
+    """
+    for key in keys:
+        try:
+            data = data[key]
+        except (KeyError, TypeError, IndexError):
+            return default
+    return data
+
+
+with open('C:\\!Равшан_работа\\OMT Consult\\Карты\\inn.txt', 'r',  encoding='utf-8') as w:
+    list_inn = []
     for line in w:
-        a.append(line.strip('\n'))
+        list_inn.append(line.strip('\n'))
 
 token = "95f4180a67114d0806e5b9afe92998e051eb8831"
 dadata = Dadata(token)
-d = {}
+dict_inn = {}
 
-for i in a:
+progress_bar = tqdm(desc="status inn parse", total=len(list_inn), leave=False)
+for i in list_inn:
     result = dadata.find_by_id("party", query=i)
-    d[i] = {}
-    try:
-        d[i]['name'] = result[0]['value']
-    except Exception:
-        d[i]['name'] = ''
-    try:
-        d[i]['type'] = result[0]['data']['type']
-    except Exception:
-        d[i]['type'] = ''
-    try:
-        d[i]['address'] = result[0]['data']['address']['unrestricted_value']
-    except Exception:
-        d[i]['address'] = ''
-    try:
-        d[i]['phone'] = result[0]['data']['phones']
-    except Exception:
-        d[i]['phone'] = ''
-    try:
-        d[i]['email'] = result[0]['data']['emails']
-    except Exception:
-        d[i]['email'] = ''
-    if d[i]['type'] == 'INDIVIDUAL':
-        try:
-            d[i]['full_name_director'] = str(result[0]['data']['fio']['surname'] + ' ' + result[0]['data']['fio']['name']\
-                                         + ' ' + result[0]['data']['fio']['patronymic'])
-        except Exception:
-            d[i]['full_name_director'] = ''
-    elif d[i]['type'] == 'LEGAL':
-        try:
-            d[i]['full_name_director'] = result[0]['data']['management']['name']
-        except Exception:
-            d[i]['full_name_director'] = ''
-    try:
-        d[i]['status_itn'] = result[0]['data']['state']['status']
-    except Exception:
-        d[i]['status_itn'] = ''
-    try:
-        d[i]['updated_date'] = datetime.utcfromtimestamp(int(str(result[0]['data']['state']['actuality_date'])[:-3])).\
-                                strftime('%Y-%m-%d %H:%M:%S')
-    except Exception:
-        d[i]['updated_date'] = ''
-    try:
-        d[i]['okved'] = result[0]['data']['okved']
-    except Exception:
-        d[i]['okved'] = ''
+    dict_inn[i] = {
+        'name': safe_get(result, 0, 'value'),
+        'type': safe_get(result, 0, 'data', 'type'),
+        'address': safe_get(result, 0, 'data', 'address', 'unrestricted_value'),
+        'phone': safe_get(result, 0, 'data', 'phones'),
+        'email': safe_get(result, 0, 'data', 'emails'),
+        'status_itn': safe_get(result, 0, 'data', 'state', 'status'),
+        'updated_date': safe_get(
+            result, 0, 'data', 'state', 'actuality_date',
+            default=None
+        ),
+        'okved': safe_get(result, 0, 'data', 'okved'),
+        'kpp': safe_get(result, 0, 'data', 'kpp'),
+    }
 
-# with open('C:\\!Равшан_работа\\OMT Consult\\Карты\\data_inn.json', 'w', encoding='utf-8-sig') as f:
-#     json.dump(d, f, ensure_ascii=False, indent=4)
-df = pd.DataFrame.from_dict(d, orient='index')
+    if dict_inn[i]['updated_date']:
+        try:
+            dict_inn[i]['updated_date'] = datetime.utcfromtimestamp(
+                int(str(dict_inn[i]['updated_date'])[:-3])
+            ).strftime('%Y-%m-%d %H:%M:%S')
+        except Exception:
+            dict_inn[i]['updated_date'] = ''
+
+    if dict_inn[i]['type'] == 'INDIVIDUAL':
+        dict_inn[i]['full_name_director'] = f"{safe_get(result, 0, 'data', 'fio', 'surname')} " \
+                                     f"{safe_get(result, 0, 'data', 'fio', 'name')} " \
+                                     f"{safe_get(result, 0, 'data', 'fio', 'patronymic')}".strip()
+    elif dict_inn[i]['type'] == 'LEGAL':
+        dict_inn[i]['full_name_director'] = safe_get(result, 0, 'data', 'management', 'name')
+    else:
+        dict_inn[i]['full_name_director'] = ''
+
+    progress_bar.update(1)
+
+progress_bar.close()
+dadata.close()
+
+df = pd.DataFrame.from_dict(dict_inn, orient='index')
 dict_status = {
     'ACTIVE': 'действующая',
     'LIQUIDATING': 'ликвидируется',
@@ -78,6 +84,7 @@ dict_opf = {
 df.type = df[df['type'] != ''].type.map(lambda x: dict_opf[x])
 df.status_itn = df[df['status_itn'] != ''].status_itn.map(lambda x: dict_status[x])
 
+now = datetime.now().strftime("%Y-%m-%d_%H%M%S")
 df.reset_index(level=0, inplace=True)
 df.rename(columns={'index': 'itn'}, inplace=True)
-df.to_excel('C:\\!Равшан_работа\\OMT Consult\\Карты\\data_inn_112.xlsx', encoding='utf-8-sig', index=False)
+df.to_excel(f'C:\\!Равшан_работа\\OMT Consult\\Карты\\data_inn_data_{now}.xlsx', index=False)
